91fb6aa12c31e9cfe68291bb8c752341f27dcaf4
Artem <info@3dartix.ru>
2020-04-18 16:34:33 +0200

    dz2

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+﻿document.write(	'<h1>Магазин &ldquo;Кошки-картошки&rdquo;</h1>'+
+				'<ul>'+
+					'<li><a href="index.html">Главная</a></li>'+
+					'<li><a href="catalog.html">Каталог</a></li>'+
+					'<li><a href="contacts.html">Контакты</a></li>'+
+				'</ul>'
+		);
