36a1ca00d577787de1ce1508cab528d86337a750
Artem <info@3dartix.ru>
2020-04-23 11:38:20 +0200

    dz3

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
-﻿document.write(	'<h1>Магазин &ldquo;Кошки-картошки&rdquo;</h1>'+
+﻿document.write(	'<div class="menu">'+
+				'<h1>Магазин &ldquo;Кошки-картошки&rdquo;</h1>'+
 				'<ul>'+
 					'<li><a href="index.html">Главная</a></li>'+
 					'<li><a href="catalog.html">Каталог</a></li>'+
 					'<li><a href="contacts.html">Контакты</a></li>'+
-				'</ul>'
+				'</ul>'+
+				'</div>'
 		);
